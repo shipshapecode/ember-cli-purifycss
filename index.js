978a5d8205a2c8cc19440e53733745f068d1ad03
Robert Wagner <rwwagner90@gmail.com>
2017-11-06 22:04:12 -0500

    First step, grabbing css file

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* eslint-env node */
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const purify = require('purify-css');
+var BroccoliDebug = require('broccoli-debug');
+
 module.exports = {
-  name: 'ember-cli-purifycss'
+  name: 'ember-cli-purifycss',
+  postprocessTree(type, tree) {
+    if (type !== 'all') {
+      return tree;
+    }
+
+    const { name } = this._findHost();
+
+    return new BroccoliDebug(new Funnel(tree, {
+      srcDir: 'assets',
+      include: [`${name}.css`]
+    }), `ember-cli-purifycss`);
+
+
+    // return mergeTrees([
+    //     tree,
+    //     purify(/*css and template tree output needed here*/)
+    //   ],
+    //   {
+    //     overwrite: true
+    //   });
+  }
 };
